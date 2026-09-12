0dab421e6af151aacafe6a907f122c3735971258
Panaccess Technologies India Pvt Ltd <panaccesstechnologies@gmail.com>
2026-09-12 10:04:22 +0530

    Add portable socket helper for multicast interface selection

---
+++ internal/transport/udp_multicast_unix.go
@@ -0,0 +1,19 @@
+//go:build linux || darwin || freebsd || netbsd || openbsd
+
+package transport
+
+import (
+	"fmt"
+	"net"
+	"syscall"
+)
+
+func setIPv4MulticastInterfaceFD(fd int, ip net.IP) error {
+	ip4 := ip.To4()
+	if ip4 == nil {
+		return fmt.Errorf("IPv4 multicast interface address is required")
+	}
+	var addr [4]byte
+	copy(addr[:], ip4)
+	return syscall.SetsockoptInet4Addr(fd, syscall.IPPROTO_IP, syscall.IP_MULTICAST_IF, addr)
+}
